fbeb5d59bc106758bb6a635e9c39bf24712ecfa9
Ondine Grosser <ondineg17@gmail.com>
2017-11-13 15:16:16 -0500

    defining function hanoi

---
+++ code.py
@@ -0,0 +1,17 @@
+def hanoi(n, begin, end, intermidiate):
+        n = int(input('Enter number of disks: '))
+        begin = [range(n)]
+        end = []
+        intermidiate = []
+        if n > 0:
+            subsq_n = begin.pop(range(1:))
+            intermediate = intermidiate.append(subsq_n)
+            base_n = begin(0)
+        for base_n in begin:
+            base_n = begin.pop(base_n)
+            end = [base_n]
+        if (intermediate = intermidiate.append(subsq_n)) and (end = begin.pop(base_n)):
+            subsq_n = intermediate.pop(subsq_n)
+            end = end.append(subsq_n)
+
+        print(end)
