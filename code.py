9b8ddb9e6955efbccf7a7e1203ae74b0c8ff4fda
Ondine Grosser <ondineg17@gmail.com>
2017-11-14 10:46:30 -0500

    hanoi function

--- code.py
+++ code.py
@@ -3,14 +3,17 @@ def hanoi(n, begin, end, intermidiate):
         begin = [range(n)]
         end = []
         intermidiate = []
-        if n > 0:
-            subsq_n = begin.pop(range(1:))
+        if n <= 0:
+            print('Error.')
+        else:
+            subsq_n = begin.pop(1:)
             intermediate = intermidiate.append(subsq_n)
             base_n = begin(0)
-        for base_n in begin:
-            base_n = begin.pop(base_n)
-            end = [base_n]
-        if (intermediate = intermidiate.append(subsq_n)) and (end = begin.pop(base_n)):
-            subsq_n = intermediate.pop(subsq_n)
-            end = end.append(subsq_n)
-            return end
+            for base_n in begin:
+                base_n = begin.pop(base_n)
+                end = [base_n]
+            if (intermediate = intermidiate.append(subsq_n)) and (end = begin.pop(base_n)):
+                subsq_n = intermediate.pop(subsq_n)
+                end = end.append(subsq_n)
+            return
+print(end)
