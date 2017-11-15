df46dab2f400b8662a0d15b9bb9c4304f9eacf50
Ondine Grosser <ondineg17@gmail.com>
2017-11-15 16:21:19 -0500

    hanoi function

--- code.py
+++ code.py
@@ -1,20 +1,18 @@
-def hanoi(n):
+def hanoi(n, begin, end, inter):
         n = int(input('Enter number of disks: '))
-        if n <= 0:
+        begin = []
+        begin.append(n)
+        for n in begin:
+            if n == 1:
+                break
+            else:
+                begin.append(n)
+                n -= 1
+        if n < 0:
             print('Error.')
         else:
-            begin = [range(n)] #starting pole
-            end = [] #end pole
-            inter = [] #intermediate pole
-            subsq_n = begin.pop(range(1,n)) #everything except the 1st disk
-            inter = inter[subsq_n]
-            base_n = begin(0)
-            for base_n in begin:
-                base_n = begin.pop(base_n)
-                end = end[base_n]
-            if (inter[subsq_n]) and (end[base_n]):
-                subsq_n = inter.pop(subsq_n)
-                end = end.append(subsq_n)
-            return end
-        print(end)
-
+            hanoi(n - 1, begin, end, inter)
+        if begin[0]:
+            inter.append(begin.pop())
+        hanoi(n - 1, inter, begin, end)
+        return end
